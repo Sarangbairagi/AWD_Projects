ba20339b18a629311e15304f2b3bdd97bbe089e9
Sarang <bairagiv401@gmail.com>
2024-02-02 04:56:12 +0530

    Angular Projects

---
+++ Marvellous Binding/Interpolation/app/demo/demo.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent 
+{
+  public Technology = "angular";
+
+  public Display () : string
+  {
+      var i : string = "MarvellousInfosystems"
+      return i;
+  }
+}
